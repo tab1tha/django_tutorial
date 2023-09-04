56c483c8100442ae80ac95b1b02a54eb301a72aa
tab1tha <tambe.tabitha@socialfinance.org.uk>
2023-09-04 17:58:24 +0100

    models, migrations, and admin view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
+    "sfdata.apps.SfdataConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC + 1 "
+TIME_ZONE = "Europe/London "
 
 USE_I18N = True
 
--- mysite/sfdata/models.py
+++ mysite/sfdata/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    file_name = models.CharField(max_length=200)
+    date_uploaded = models.DateTimeField(db_comment="date and time when file was uploaded")
